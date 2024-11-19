912490ac81c0f1e371a5babf65bcdc4b919740fa
victorgargor <victor.gargor@educa.jcyl.es>
2024-11-19 09:22:19 +0100

    Primer Commit del Tema 5 19/11/2024

---
+++ config/ConfDBPDO.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Fichero de configuración para la conexión a la BD 
+ * @author Víctor García Gordón
+ * @version Fecha de última modificación 12/11/2024
+ */
+
+// Conexión DB Entorno Desarrollo Clase
+//PDO
+define("DSN", 'mysql:host=192.168.20.19;dbname=DB202DWESProyectoTema5');// IP del servidor y nombre de la base de datos
+define("USER", 'user202DWESProyectoTema5');// Usuario para conectarse
+define("PASSWORD", 'paso');// Contraseña para conectarse
+
+/*
+// Conexión DB Entorno Desarrollo Casa
+define("DSN", 'mysql:host=192.168.0.229;dbname=DB202DWESProyectoTema5');// IP del servidor y nombre de la base de datos
+define("USER", 'adminsql');// Usuario para conectarse
+define("PASSWORD", 'P@ssword');// Contraseña para conectarse 
+ */
---
+++ indexProyectoTema5.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="webroot/css/index.css" type="text/css">
+        <title>Víctor García Gordón</title>
+    </head>
+    <body>
+        <header>      
+            <h1 id="inicio">TEMA 5 DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</h1>
+        </header>
+        <main>
+            <nav>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>Entorno de Desarrollo</th>
+                        <th>Entorno de Explotación</th>
+                    </tr>
+                    <tr>
+                        <td><a>Script creación BD</a></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><a>Script carga BD</a></td>
+                        <td></td>                   
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><a>Script borrado BD</a></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <th>Fichero de configuración de la DB (PDO)</th>                   
+                    </tr>
+                    <tr>
+                        <td></td>      
+                    </tr>
+                </table>
+                <br>
+                <table>
+                    <tr>
+                        <th>nº</th>
+                        <th>Enunciado</th>
+                        <th>Mostrar</th>
+                        <th>Ejecutar</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>Mostrar el contenido de las variables superglobales y phpinfo()</td>
+                        <td></td>
+                        <td></td>                       
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Desarrollo de un control de acceso con identificación del usuario basado en la función header()</td>
+                        <td></td>
+                        <td></td>                      
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Desarrollo de un control de acceso con identificación del usuario basado en la función header() y
+                            en el uso de una tabla “Usuario” de la base de datos. (PDO)</td>
+                        <td></td>
+                        <td></td>                       
+                    </tr>
+                </table>
+            </nav>       
+        </main>
+        <footer>
+            <div>
+                <a href="/index.html">Víctor García Gordón</a>
+                <a href="/202DWESProyectoDWES/indexProyectoDWES.php">DWES</a>
+                <a target="blank" href="doc/curriculum.pdf"><img src="doc/curriculum.jpg" alt="curriculum"></a>
+                <a target="blank" href="https://github.com/victorgargor/202DWESProyectoTema5"><img src="doc/github.png" alt="github"></a>
+                <a target="blank" href="https://github.com">Web Imitada</a>
+            </div>
+        </footer>
+    </body>
+</html>
+
+
+
+
